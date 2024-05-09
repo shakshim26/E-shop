ea33ab69c73c8442b91d9eeefdd729f934a09ae2
shakshim26 <155253350+shakshim26@users.noreply.github.com>
2024-05-09 23:52:26 +0530

    initial commit

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,29 +1,29 @@
 
 // Special Countdown 
-// let daysItem = document.querySelector("#days");
-// let hoursItem = document.querySelector("#hours");
-// let minItem = document.querySelector("#min");
-// let secItem = document.querySelector("#sec");
+let daysItem = document.querySelector("#days");
+let hoursItem = document.querySelector("#hours");
+let minItem = document.querySelector("#min");
+let secItem = document.querySelector("#sec");
 
-// let countDown = () => {
-//     let futureDate = new Date("7 July 2024");
-//     let currentDate = new Date();
-//     let myDate = futureDate - currentDate;
+let countDown = () => {
+    let futureDate = new Date("7 July 2024");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
 
-//     let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
-//     let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
-//     let min = Math.floor(myDate / 1000 / 60) % 60;
-//     let sec = Math.floor(myDate / 1000) % 60;
+    let days = Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours = Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min = Math.floor(myDate / 1000 / 60) % 60;
+    let sec = Math.floor(myDate / 1000) % 60;
 
-//     daysItem.innerHTML = days;
-//     hoursItem.innerHTML = hours;
-//     minItem.innerHTML = min;
-//     secItem.innerHTML = sec;
-// }
+    daysItem.innerHTML = days;
+    hoursItem.innerHTML = hours;
+    minItem.innerHTML = min;
+    secItem.innerHTML = sec;
+}
 
-// countDown()
+countDown()
 
-// setInterval(countDown, 1000)
+setInterval(countDown, 1000)
 
 // Scroll Back To Top 
 function scrollTopBack(){
